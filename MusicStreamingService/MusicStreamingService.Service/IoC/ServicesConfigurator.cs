6d6d7b0b8dd289899234ad585294c1f5a204af39
kanatnikovmaxim@yandex.ru <kanatnikovmaxim@yandex.ru>
2025-04-05 18:23:11 +0300

    configure repositories

---
+++ MusicStreamingService/MusicStreamingService.Service/IoC/ServicesConfigurator.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicStreamingService.DataAccess.Context;
+using MusicStreamingService.DataAccess.Repositories;
+using MusicStreamingService.DataAccess.Repositories.Interfaces;
+using MusicStreamingService.Service.Settings;
+
+namespace MusicStreamingService.Service.IoC;
+
+public static class ServicesConfigurator
+{
+    public static void ConfigureServices(IServiceCollection services, MusicServiceSettings settings)
+    {
+        services.AddScoped<ISongsRepository>(x =>
+            new SongsRepository(x.GetRequiredService<IDbContextFactory<MusicServiceDbContext>>()));
+        services.AddScoped<IAlbumsRepository>(x =>
+            new AlbumsRepository(x.GetRequiredService<IDbContextFactory<MusicServiceDbContext>>()));
+        services.AddScoped<IArtistsRepository>(x =>
+            new ArtistsRepository(x.GetRequiredService<IDbContextFactory<MusicServiceDbContext>>()));
+        services.AddScoped<IUsersRepository>(x =>
+            new UsersRepository(x.GetRequiredService<IDbContextFactory<MusicServiceDbContext>>()));
+    }
+}
